4b2d91d4f7a66de6f29bfbde5d249555e12812d8
Houssem Marzougui <marzouguih86@gmail.com>
2021-11-02 01:10:47 -0700

    second commit

---
+++ src/components/Task.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{ task.text }} <i @click="delete task.id" class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: {
+      type: Object,
+    },
+  },
+  methods: {
+    delete(id) {
+      this.$emit("delete", id);
+    },
+  },
+};
+</script>
+<style scoped>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
